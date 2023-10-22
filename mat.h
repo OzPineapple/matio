eb81c8f120d47392a8818e8fb77d3d24f0df9d90
Jozyelotl Joestar <ozielcortespinna@gmail.com>
2023-10-22 08:25:16 -0600

    mv read.h to mat.h for mat definitions

---
+++ mat.h
@@ -0,0 +1,53 @@
+#ifndef MAT_H
+#define MAT_H
+
+#define ZCHUNK 1024*160
+
+#define FOREACH_MATARRAY( FUN ) \
+	FUN( UNKHOW_ARRAY   ) \
+	FUN( mxCELL_CLASS   ) \
+	FUN( mxSTRUCT_CLASS ) \
+	FUN( mxOBJECT_CLASS ) \
+	FUN( mxCHAR_CLASS   ) \
+	FUN( mxSPARSE_CLASS ) \
+	FUN( mxDOUBLE_CLASS ) \
+	FUN( mxSINGLE_CLASS ) \
+	FUN( mxINT8_CLASS   ) \
+	FUN( mxUINT8_CLASS  ) \
+	FUN( mxINT16_CLASS  ) \
+	FUN( mxUINT16_CLASS ) \
+	FUN( mxINT32_CLASS  ) \
+	FUN( mxUINT32_CLASS ) \
+	FUN( mxINT64_CLASS  ) \
+	FUN( mxUINT64_CLASS )
+
+#define FOREACH_MATTYPE( FUN ) \
+	FUN( UNKHOW_TYPE  ) \
+	FUN( miINT8       ) \
+	FUN( miUINT8      ) \
+	FUN( miINT16      ) \
+	FUN( miUINT16     ) \
+	FUN( miINT32      ) \
+	FUN( miUINT32     ) \
+	FUN( miSINGLE     ) \
+	FUN( Reserved8    ) \
+	FUN( miDOUBLE     ) \
+	FUN( Reserved10   ) \
+	FUN( Reserved11   ) \
+	FUN( miINT64      ) \
+	FUN( miUINT64     ) \
+	FUN( miMATRIX     ) \
+	FUN( miCOMPRESSED ) \
+	FUN( miUTF8       ) \
+	FUN( miUTF16      ) \
+	FUN( miUTF32      ) 
+
+#define GENERATE_ENUM( ENUM )    ENUM,
+#define GENERATE_STRING( ENUM ) #ENUM,
+
+enum mat_data_type  { FOREACH_MATTYPE(  GENERATE_ENUM ) };
+enum mat_array_type { FOREACH_MATARRAY( GENERATE_ENUM ) };
+static const char * mat_data_string[]  = { FOREACH_MATTYPE(  GENERATE_STRING ) };
+static const char * mat_array_string[] = { FOREACH_MATARRAY( GENERATE_STRING ) };
+
+#endif // MAT_H
